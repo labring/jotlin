a8255d54cc1d1aec9c43b1f720806460b40991e2
Alex Lee <3076438032@qq.com>
2023-11-20 17:13:02 +0800

    feat: user setting

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,36 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { useUser } from '@clerk/clerk-react'
+import { PlusCircle } from 'lucide-react'
+import Image from 'next/image'
+
 const DocumentsPage = () => {
-  return <div>Documents page</div>
+  const { user } = useUser()
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Jotlin
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  )
 }
 
 export default DocumentsPage
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -3,6 +3,7 @@ import { ChevronsLeft, MenuIcon } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import { ElementRef, useRef, useState, useEffect } from 'react'
 import { useMediaQuery } from 'usehooks-ts'
+import UserItem from './user-item'
 
 const Navigation = () => {
   const pathname = usePathname()
@@ -113,7 +114,7 @@ const Navigation = () => {
           <ChevronsLeft className="h-6 w-6" />
         </div>
         <div>
-          <p>Action items</p>
+          <UserItem />
         </div>
         <div className="mt-4">
           <p>Documents</p>
