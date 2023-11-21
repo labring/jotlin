d37cdfff87e403dd3f8a777fa57f96990e9f7451
Alex Lee <3076438032@qq.com>
2023-11-21 23:10:46 +0800

    feat: archive functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,11 +1,26 @@
 'use client'
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
 import { Skeleton } from '@/components/ui/skeleton'
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { cn } from '@/lib/utils'
+import { useUser } from '@clerk/clerk-react'
+import { DropdownMenuSeparator } from '@radix-ui/react-dropdown-menu'
 import { useMutation } from 'convex/react'
-import { ChevronDown, ChevronRight, LucideIcon, Plus } from 'lucide-react'
+import {
+  ChevronDown,
+  ChevronRight,
+  LucideIcon,
+  MoreHorizontal,
+  Plus,
+  Trash,
+} from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
@@ -34,8 +49,22 @@ const Item = ({
   onClick,
   icon: Icon,
 }: ItemProps) => {
+  const { user } = useUser()
   const create = useMutation(api.documents.create)
   const router = useRouter()
+  const archive = useMutation(api.documents.archive)
+
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation()
+    if (!id) return
+    const promise = archive({ id })
+
+    toast.promise(promise, {
+      loading: 'Moving to trash...',
+      success: 'Note moved to trash!',
+      error: 'Failed to archive note.',
+    })
+  }
   //切换展开状态
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -54,7 +83,7 @@ const Item = ({
         if (!expanded) {
           onExpand?.()
         }
-        // router.push(`/documents/${documentId}`)
+        router.push(`/documents/${documentId}`)
       }
     )
     toast.promise(promise, {
@@ -91,15 +120,39 @@ const Item = ({
       )}
       {/* 文档名字 */}
       <span className="truncate">{label}</span>
-      {/* 搜索 */}
+      {/* 当是搜索时呈现 */}
       {isSearch && (
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">Control</span>K
         </kbd>
       )}
-      {/* 加号符号->悬浮创建新子文档 */}
+      {/* 右侧功能区 */}
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2">
+          {/*下拉菜单，三个小点 */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild onClick={(e) => e.stopPropagation()}>
+              <div
+                role="button"
+                className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600">
+                <MoreHorizontal className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              className="w-60"
+              align="start"
+              side="right"
+              forceMount>
+              <DropdownMenuItem onClick={onArchive}>
+                <Trash className="h-4 w-4 mr-2" />
+                Delete
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <div className="text-xs text-muted-foreground p-2">
+                Last edited by:{user?.fullName}
+              </div>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <div
             role="button"
             onClick={onCreate}
