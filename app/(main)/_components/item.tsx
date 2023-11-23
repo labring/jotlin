44e9731bbbfea9d57db1ea3f489905571d438e4a
Alex Lee <3076438032@qq.com>
2023-11-23 11:44:47 +0800

    feat: preview functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -117,7 +117,7 @@ const Item = ({
       {documentIcon ? (
         <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+        <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
       )}
       {/* 文档名字 */}
       <span className="truncate">{label}</span>
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -8,6 +8,7 @@ import { MenuIcon } from 'lucide-react'
 import Title from './title'
 import Banner from './banner'
 import Menu from './menu'
+import Publish from './publish'
 
 interface NavbarProps {
   isCollapsed: boolean
@@ -46,6 +47,7 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
