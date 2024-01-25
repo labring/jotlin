38e59287cd6241a24337cd7245791d5cda19e2d8
Alex Lee <3076438032@qq.com>
2024-01-25 16:27:55 +0800

    feat: quote block

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from 'next'
 import { Toaster } from 'sonner'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
 import { ModalProvider } from '@/components/providers/modal-provider'
 import { EdgeStoreProvider } from '../lib/edgestore'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
