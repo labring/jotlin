1afc638707d063d36bb578625d2f694789132559
Alex Lee <3076438032@qq.com>
2024-01-30 14:32:12 +0800

    fix: cannot invite yourself

--- convex/documents.ts
+++ convex/documents.ts
@@ -231,6 +231,7 @@ export const getSearch = query({
   },
 })
 
+// function: 根据id获取文档名字
 export const getById = query({
   args: { documentId: v.id('documents') },
   handler: async (ctx, args) => {
