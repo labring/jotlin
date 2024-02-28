7968483971f38bbdbf28ec09e5a33e941c273fe5
Alex Lee <3076438032@qq.com>
2024-02-28 17:17:38 +0800

    refactor: sign out

--- app/(marking)/components/navbar.tsx
+++ app/(marking)/components/navbar.tsx
@@ -10,9 +10,15 @@ import Link from 'next/link'
 import { useAuth } from '@/stores/use-auth'
 import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import { useSession } from '@/hooks/use-session'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { ArrowRightFromLine, Settings } from 'lucide-react'
 
 const Navbar = () => {
-  const { isAuthenticated, isLoading, user } = useSession()
+  const { isAuthenticated, isLoading, user, signOut } = useSession()
   const scrolled = useScrollTop()
   const authModal = useAuth()
   return (
@@ -39,11 +45,37 @@ const Navbar = () => {
             <Button variant="ghost" size="sm" asChild>
               <Link href="/documents">Enter Jotlin</Link>
             </Button>
-            <Avatar>
-              <AvatarImage
-                src={user?.imageUrl}
-                alt={user?.username}></AvatarImage>
-            </Avatar>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Avatar>
+                  <AvatarImage src={user?.imageUrl} alt={user?.username} />
+                </Avatar>
+              </PopoverTrigger>
+              <PopoverContent className="mr-16 grid w-80 grid-cols-3 items-center gap-x-1 gap-y-4">
+                <div className="flex items-center justify-center">
+                  <Avatar>
+                    <AvatarImage src={user?.imageUrl} alt={user?.username} />
+                  </Avatar>
+                </div>
+                <div className="col-span-2 font-medium">
+                  <span>{user?.username}</span>
+                </div>
+                <div className="flex items-center justify-center ">
+                  <Settings className="h-4 w-4 text-stone-400" />
+                </div>
+                <div className="col-span-2 cursor-pointer">
+                  <span className="font-normal text-stone-500">
+                    Manage account
+                  </span>
+                </div>
+                <div className="flex items-center justify-center ">
+                  <ArrowRightFromLine className="h-4 w-4 text-stone-400" />
+                </div>
+                <div className="col-span-2 cursor-pointer" onClick={signOut}>
+                  <span className="font-normal text-stone-500">Sign out</span>
+                </div>
+              </PopoverContent>
+            </Popover>
           </>
         )}
         <ModeToggle />
