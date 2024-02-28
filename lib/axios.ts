d365ad3dc430b50a491aa024ff39858847c0d691
Alex Lee <3076438032@qq.com>
2024-02-28 14:26:06 +0800

    fix: invalid hook call

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
 import type { InternalAxiosRequestConfig } from 'axios'
-import { useSession } from '../hooks/use-session'
 
 // 拦截 request，添加 token 凭据
 axios.interceptors.request.use((config: InternalAxiosRequestConfig) => {
-  if (!config.headers?.token) {
-    const { token } = useSession()
-    if (token) config.headers = Object.assign({}, config.headers, { token })
+  if (!config.headers?.Authorization) {
+    const token = localStorage.getItem('token')
+    if (token)
+      config.headers = Object.assign({}, config.headers, {
+        Authorization: `Bearer ${token}`,
+      })
   }
   return config
 })
