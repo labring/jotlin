9047d9d89f77336e65417be5b4dfa5cdcb8e8595
Alex Lee <3076438032@qq.com>
2024-04-12 19:30:14 +0800

    chore: update marketing page

--- app/(marking)/components/heading.tsx
+++ app/(marking)/components/heading.tsx
@@ -27,12 +27,12 @@ const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
       <h1 className="text-3xl font-bold sm:text-5xl md:text-6xl">
-        Your Ideas,Documents,&Plans. Unified. Welcome to&nbsp;
-        <span className="underline">Jotlin</span>
+        Your Ideas,Documents,&Plans. Unified. With LLM.
       </h1>
       <h3 className="text-base font-medium sm:text-xl md:text-2xl">
-        Jotlin is the connected workspace where <br />
-        better ,faster work happens.
+        Jotlin is the workspace for you to unlock the potential
+        <br />
+        of LLM to writing, planning, and collaborating.
       </h3>
       {/* 加载动画 */}
       {isLoading && (
