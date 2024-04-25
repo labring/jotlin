4b1bcab2490b84011059d0348e0ab6d77a358341
Alex Lee <3076438032@qq.com>
2024-04-25 16:55:59 +0800

    chore: laf api update

--- laf-cloud/functions/__websocket__.ts
+++ laf-cloud/functions/__websocket__.ts
@@ -1,24 +1,128 @@
-const setupWSConnection = require('@/utils').setupWSConnection
+
+import * as map from 'lib0/map'
+
+const wsReadyStateConnecting = 0
+const wsReadyStateOpen = 1
+const wsReadyStateClosing = 2 // eslint-disable-line
+const wsReadyStateClosed = 3 // eslint-disable-line
+
+const pingTimeout = 30000
+
+
+/**
+ * Map froms topic-name to set of subscribed clients.
+ * @type {Map<string, Set<any>>}
+ */
+const topics = new Map()
+
+/**
+ * @param {any} conn
+ * @param {object} message
+ */
+const send = (conn, message) => {
+  if (conn.readyState !== wsReadyStateConnecting && conn.readyState !== wsReadyStateOpen) {
+    conn.close()
+  }
+  try {
+    conn.send(JSON.stringify(message))
+  } catch (e) {
+    conn.close()
+  }
+}
+
+const onconnection = conn => {
+  /**
+   * @type {Set<string>}
+   */
+  const subscribedTopics = new Set()
+  let closed = false
+  // Check if connection is still alive
+  let pongReceived = true
+  const pingInterval = setInterval(() => {
+    if (!pongReceived) {
+      conn.close()
+      clearInterval(pingInterval)
+    } else {
+      pongReceived = false
+      try {
+        conn.ping()
+      } catch (e) {
+        conn.close()
+      }
+    }
+  }, pingTimeout)
+  conn.on('pong', () => {
+    pongReceived = true
+  })
+  conn.on('close', () => {
+    subscribedTopics.forEach(topicName => {
+      const subs = topics.get(topicName) || new Set()
+      subs.delete(conn)
+      if (subs.size === 0) {
+        topics.delete(topicName)
+      }
+    })
+    subscribedTopics.clear()
+    closed = true
+  })
+  conn.on('message', /** @param {object} message */ message => {
+    if (typeof message === 'string' || message instanceof Buffer) {
+      message = JSON.parse(message)
+    }
+    if (message && message.type && !closed) {
+      switch (message.type) {
+        case 'subscribe':
+          /** @type {Array<string>} */ (message.topics || []).forEach(topicName => {
+            if (typeof topicName === 'string') {
+              // add conn to topic
+              const topic = map.setIfUndefined(topics, topicName, () => new Set())
+              topic.add(conn)
+              // add topic to conn
+              subscribedTopics.add(topicName)
+            }
+          })
+          break
+        case 'unsubscribe':
+          /** @type {Array<string>} */ (message.topics || []).forEach(topicName => {
+            const subs = topics.get(topicName)
+            if (subs) {
+              subs.delete(conn)
+            }
+          })
+          break
+        case 'publish':
+          if (message.topic) {
+            const receivers = topics.get(message.topic)
+            if (receivers) {
+              message.clients = receivers.size
+              receivers.forEach(receiver =>
+                send(receiver, message)
+              )
+            }
+          }
+          break
+        case 'ping':
+          send(conn, { type: 'pong' })
+      }
+    }
+  })
+}
 
 export async function main(ctx: FunctionContext) {
+
   if (ctx.method === 'WebSocket:connection') {
-    const conn = ctx.socket
-    console.log(ctx)
-    setupWSConnection(conn, ctx.request)
-    ctx.socket.send('hi connection succeed')
+    onconnection(ctx.socket)
   }
+
   // if (ctx.method === 'WebSocket:message') {
-  //   const { data } = ctx.params
-  //   ctx.socket.send(data.toString())
+  
   // }
 
-  if (ctx.method === 'WebSocket:error') {
-    const error = ctx.params
-    console.log(error)
-  }
+  // if (ctx.method === 'WebSocket:error') {
+  //   const error = ctx.params
+  //   console.log(error)
+  // }
 
-  if (ctx.method === 'WebSocket:close') {
-    const { code, reason } = ctx.params
-    console.log(code, reason)
-  }
+  // if (ctx.method === 'WebSocket:close') {
+  // }
 }
