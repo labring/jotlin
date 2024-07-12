310e8ae82b946570820b7ddc7e0536d64d7ab75c
Alex Lee <3076438032@qq.com>
2024-07-12 17:17:43 +0800

    feat: markdown import/export

--- app/(main)/components/menu.tsx
+++ app/(main)/components/menu.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { toast } from 'sonner'
+import { mutate } from 'swr'
 import { useRouter } from 'next/navigation'
-import { MoreHorizontal, Trash } from 'lucide-react'
+import { MoreHorizontal, Trash, FolderUp, Download } from 'lucide-react'
 
 import {
   DropdownMenu,
@@ -13,9 +14,11 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { Button } from '@/components/ui/button'
 import { Skeleton } from '@/components/ui/skeleton'
+import { useCreateBlockNote } from '@blocknote/react'
 
-import { archive } from '@/api/document'
+import { archive, update } from '@/api/document'
 import { useSession } from '@/hooks/use-session'
+import { useDocument } from '@/stores/use-document'
 
 interface MenuProps {
   documentId: string
@@ -24,17 +27,74 @@ interface MenuProps {
 const Menu = ({ documentId }: MenuProps) => {
   const router = useRouter()
   const { user } = useSession()
+  const editor = useCreateBlockNote()
+  const { document: currentDocument } = useDocument()
 
   const onArchive = async () => {
     try {
       toast.loading('Moving to trash...')
-      const response = archive(documentId)
+      archive(documentId)
+      mutate(
+        (key) =>
+          typeof key === 'string' && key.startsWith('/api/document/sidebar')
+      )
       toast.success('Note moved to trash.')
       router.push('/documents')
     } catch (error) {
       toast.error('Failed to archive note.')
     }
   }
+
+  const onImport = async () => {
+    const input = document.createElement('input')
+    input.type = 'file'
+    input.accept = '.md'
+    input.onchange = async (event) => {
+      const file = (event.target as HTMLInputElement).files?.[0]
+      if (file) {
+        const title = file.name.replace('.md', '')
+        const markdownContent = await file.text()
+        const blocks = await editor.tryParseMarkdownToBlocks(markdownContent)
+        try {
+          toast.loading('Importing note...')
+          await update({
+            _id: documentId,
+            title,
+            content: JSON.stringify(blocks),
+          })
+          mutate(
+            (key) =>
+              typeof key === 'string' &&
+              key.startsWith('/api/document/get-document-by-id')
+          )
+          toast.success('Note imported.')
+        } catch (error) {
+          toast.error('Failed to import note.')
+        }
+      }
+    }
+    input.click()
+  }
+
+  const onExport = async () => {
+    try {
+      toast.loading('Exporting note...')
+      const title = currentDocument?.title || 'Untitled'
+      const blockContent = JSON.parse(currentDocument?.content as string) || []
+      const markdownContent = await editor.blocksToMarkdownLossy(blockContent)
+
+      const blob = new Blob([markdownContent], { type: 'text/markdown' })
+      const url = URL.createObjectURL(blob)
+      const a = document.createElement('a')
+      a.href = url
+      a.download = `${title}.md`
+      a.click()
+      toast.success('Note exported.')
+    } catch (error) {
+      toast.error('Failed to export note.')
+    }
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -47,6 +107,14 @@ const Menu = ({ documentId }: MenuProps) => {
         align="end"
         alignOffset={8}
         forceMount>
+        <DropdownMenuItem onClick={onImport}>
+          <FolderUp className="mr-2 h-4 w-4" />
+          Import from md file
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={onExport}>
+          <Download className="mr-2 h-4 w-4" />
+          Export to md file
+        </DropdownMenuItem>
         <DropdownMenuItem onClick={onArchive}>
           <Trash className="mr-2 h-4 w-4" />
           Delete
